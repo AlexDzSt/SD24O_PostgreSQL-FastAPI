040893a4ea8ef8ab65d1b2986256348b7a3ac381
Alexis Diaz <alexis.diaz@cua.uam.mx>
2024-12-10 12:26:21 -0600

    PUT

--- api.py
+++ api.py
@@ -4,9 +4,10 @@ from pydantic import BaseModel
 import shutil
 import os
 import uuid
-import orm.repo as repo
+import orm.repo as repo #funciones para hacer consultas a la BD
+import orm.esquemas as esquemas
 from sqlalchemy.orm import Session
-from orm.config import generador_sesion
+from orm.config import generador_sesion #generador de sesiones
 
 # creación del servidor
 app = FastAPI()
@@ -50,7 +51,7 @@ def hola_mundo():
 
     return respuesta
 
-# USUARIOS
+
 @app.get("/usuarios/{id}/compras/{id_compra}")
 def compras_usuario_por_id(id: int, id_compra: int):
     print("buscando compra con id:", id_compra, " del usuario con id:", id)
@@ -64,15 +65,28 @@ def compras_usuario_por_id(id: int, id_compra: int):
     return compra
 
 @app.get("/usuarios/{id}")
-def usuario_por_id(id:int, sesion:Session=Depends(generador_sesion)):
+def usuario_por_id(id:int,sesion:Session=Depends(generador_sesion)):
     print("Api consultando usuario por id")
     return repo.usuario_por_id(sesion, id)
 
-#/usuarios?edad_min={edadmin}&edad_max={edadMax}
+@app.get("/usuarios/{id}/fotos")
+def fotos_por_id_usr(id:int,sesion:Session=Depends(generador_sesion)):
+    print("API consultando fotos del usuario ", id)
+    return repo.fotos_por_id_usuario(sesion, id)
+
+@app.get("/usuarios/{id}/compras")
+def fotos_por_id_usr(id:int,sesion:Session=Depends(generador_sesion)):
+    print("API consultando compras del usuario ", id)
+    return repo.compras_por_id_usuario(sesion, id)
+
 @app.get("/usuarios")
-def lista_usuarios(edad_min:int, edad_max:int, sesion:Session=Depends(generador_sesion)):
-    print("/usuarios?edad_min={edadMin}&edad_max={edadMax}")
-    return repo.usuario_por_rango_edad(sesion, edad_min, edad_max)
+def lista_usuarios(sesion:Session=Depends(generador_sesion)):
+    print("API consultando todos los usuarios")
+    return repo.devuelve_usuarios(sesion)
+#def lista_usuarios(*,lote:int=10,pag:int,orden:Optional[str]=None): #parametros de consulta ?lote=10&pag=1
+#    print("lote:",lote, " pag:", pag, " orden:", orden)
+#    #simulamos la consulta
+#    return usuarios
 
 @app.post("/usuarios")
 def guardar_usuario(usuario:UsuarioBase, parametro1:str):
@@ -89,43 +103,44 @@ def guardar_usuario(usuario:UsuarioBase, parametro1:str):
 
     return usr_nuevo
 
+#@app.put("/compras/{id}")
+#@app.put("/fotos/{id}")
 @app.put("/usuario/{id}")
-def actualizar_usuario(id:int, usuario:UsuarioBase):
-    #simulamos consulta
-    usr_act = usuarios[id]
-    #simulamos la actualización
-    usr_act["nombre"] = usuario.nombre
-    usr_act["edad"] = usuario.edad
-    usr_act["domicilio"] = usuario.domicilio    
-
-    return usr_act
+def actualizar_usuario(id:int,info_usuario:esquemas.UsuarioBase,sesion:Session=Depends(generador_sesion)):
+    return repo.actualiza_usuario(sesion,id,info_usuario)
     
 @app.delete("/usuario/{id}")
-def borrar_usuario(id:int):
-    #simulamos una consulta
-    if id>=0 and id< len(usuarios):
-        usuario = usuarios[id]
-    else:
-        usuario = None
-    
-    if usuario is not None:
-        usuarios.remove(usuario)
-    
-    return {"status_borrado", "ok"}
+def borrar_usuario(id:int, sesion:Session=Depends(generador_sesion)):
+    #1.- borro compras del usuario
+    #repo.borrar_compras_por_id_usuario(sesion,id)
+    #2.-borro foto del usuario
+    #repo.borrar_fotos_por_id_usuario(sesion,id)
+    repo.borra_usuario_por_id(sesion,id)
+    return {"usuario_borrado", "ok"}
 
-# COMPRAS
-#"/compras?id_usuario={id_usr}&precio={p}"
-@app.get("/compras")
-def lista_compras(id_usuario:int, precio:float, sesion:Session=Depends(generador_sesion)):
-    print("/compras?id_usuario={id_usr}&precio={p}")
-    return repo.devuelve_compras_por_usuario_precio(sesion, id_usuario, precio)
-
+## Peticiones de compras
 @app.get("/compras/{id}")
 def compra_por_id(id:int, sesion:Session=Depends(generador_sesion)):
-    print("Api consultando compra por id")
+    print("Buscando compra por id")
     return repo.compra_por_id(sesion, id)
 
-# FOTOS
+# "/compras?id_usuario={id_usr}&precio={p}"
+@app.get("/compras")
+def lista_compras(id_usuario:int,precio:float,sesion:Session=Depends(generador_sesion)):
+    print("/compras?id_usuario={id_usr}&precio={p}")
+    return repo.devuelve_compras_por_usuario_precio(sesion,id_usuario,precio)
+
+## Peticiones de fotos
+@app.get("/fotos/{id}")
+def foto_por_id(id:int, sesion:Session=Depends(generador_sesion)):
+    print("Buscando foto por id")
+    return repo.foto_por_id(sesion,id)
+
+@app.get("/fotos")
+def lista_fotos(sesion:Session=Depends(generador_sesion)):
+    print("API consultando todas las fotos")
+    return repo.devuelve_fotos(sesion)
+
 @app.post("/fotos")
 async def guardar_foto(titulo:str=Form(None), descripcion:str=Form(...), foto:UploadFile=File(...)):
     print("titulo:", titulo)
@@ -142,13 +157,3 @@ async def guardar_foto(titulo:str=Form(None), descripcion:str=Form(...), foto:Up
         imagen.write(contenido)
 
     return {"titulo":titulo, "descripcion":descripcion, "foto":foto.filename}
-
-@app.get("/fotos/{id}")
-def foto_por_id(id:int, sesion:Session=Depends(generador_sesion)):
-    print("Api consultando foto por id")
-    return repo.foto_por_id(sesion, id)
-
-@app.get("/fotos")
-def lista_fotos(sesion:Session=Depends(generador_sesion)):
-    print("Api consultando lista de fotos")
-    return repo.foto(sesion)
